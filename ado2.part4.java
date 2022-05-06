48152d149a88304a39ec96145cecbcd8675894bb
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-05-06 20:25:09 -0300

    Create ado2.part4.java

---
+++ ado2.part4.java
@@ -0,0 +1,14 @@
+mport java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+Scanner entrada = new Scanner ( System.in);
+    String nome = "";
+    System.out.println("Digite um texto");
+    nome = entrada.next();
+
+    System.out.println("Esta mensagem tem " + nome.length() + "caractehres ");
+
+  
+  }
+}
