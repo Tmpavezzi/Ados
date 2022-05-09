7bf418d1a65fa7d40623c93482f14689251dc1ff
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-05-09 20:55:40 -0300

    Create ado2.part1.java

---
+++ ado2.part1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner ;
+
+
+class Main {
+  public static void main(String[] args) {
+Double m ,x,z;
+ Scanner entrada = new Scanner (System.in);
+   System.out.println("Digie um valor ");
+    m = entrada.nextDouble();
+    System.out.println("Digie um valor ");
+   x = entrada.nextDouble();
+    System.out.println("Digie um valor ");
+    z = entrada.nextDouble();
+
+    Double Valor = m*x*z;
+    System.out.println("O valor desses produtos e  "+ Valor );
+    
+    
+
+
+
+    
+  }
+}
