bd879fa0a1ae7ce600666ac412a58e98e9f86d71
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-05-09 21:06:53 -0300

    Create ado2part2.java

---
+++ ado2part2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner ;
+
+
+class Main {
+  public static void main(String[] args) {
+int m ;
+ Scanner entrada = new Scanner (System.in);
+  System.out.print("Digie um valor ");
+    m= entrada.nextInt();
+
+  for(int c=0;c<3;c++){
+    System.out.print( "\n Mutiplico  " + (c*m));
+  }
+
+
+
+    
+  }
+}
